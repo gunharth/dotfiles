234d6d8d9272e2e746f9045c9fe000a104d66800
Gunharth Randolf <hello@gunharth.io>
2018-10-05 09:50:04 +0200

    aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,41 +1,23 @@
-# Shortcuts
-alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
-alias reloadcli="source $HOME/.zshrc"
-alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="$(brew --prefix coreutils)/libexec/gnubin/ls -ahlF --color --group-directories-first"
-weather() { curl -4 wttr.in/${1:-antwerp} }
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
-alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
-alias c="clear"
-
-# Directories
-alias dotfiles="cd $DOTFILES"
-alias library="cd $HOME/Library"
-alias sites="cd $HOME/Sites"
-
-# Laravel
-alias a="php artisan"
-alias ams="php artisan migrate:fresh --seed"
-
-# Vagrant
-alias v="vagrant global-status"
-alias vup="vagrant up"
-alias vhalt="vagrant halt"
-alias vssh="vagrant ssh"
-alias vreload="vagrant reload"
-alias vrebuild="vagrant destroy --force && vagrant up"
-
-# Docker
-alias dstop="docker stop $(docker ps -a -q)"
-alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
-alias dpurgeimages="docker rmi $(docker images -q)"
-dbuild() { docker build -t=$1 .; }
-dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
-
 # Git
-alias commit="git add . && git commit -m"
-alias gcommit="git add . && git commit"
-alias gst="git status"
-alias gc="git checkout"
-alias gd="git diff"
-alias gl="git log --oneline --decorate --color"
+alias ga="git add"
+alias gaa="git add ."
+alias gc="git commit -m"
+alias gp="git push"
+alias gs="git status"
+alias gl="git log"
+alias nah="git reset --hard && git clean -df"
+
+#homestead
+alias hu="homestead up"
+alias hh="homestead halt"
+alias hs="homestead ssh"
+
+# Laravel artisan
+alias a="php artisan"
+
+# Valet switch php version
+alias vphp71="brew unlink php56 && brew link php71 && valet stop && valet start"
+alias vphp56="brew unlink php71 && brew link php56 && valet stop && valet start"
+
+#flushDNS
+alias flushdns="sudo killall -HUP mDNSResponder"
